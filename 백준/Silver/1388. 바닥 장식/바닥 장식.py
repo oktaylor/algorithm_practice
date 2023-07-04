8a8517827c2f5b7706300a4de6bb7fc0e207a01d
강동화, Taylor <98503567+oktaylor@users.noreply.github.com>
2023-07-04 11:21:04 +0900

    [Silver IV] Title: 바닥 장식, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,26 @@
+import sys
+
+graph = []
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(sys.stdin.readline()) for _ in range(n)]
+
+def dfs(y, x):
+    if graph[y][x] == '-':
+        graph[y][x] = 0 # 방문처리
+        for i in [-1, 1]:
+            if 0 <= x+i < m and graph[y][x+i] == '-':
+                dfs(y, x+i)
+
+    elif graph[y][x] == '|':
+        graph[y][x] = 0 # 방문처리
+        for i in [-1, 1]:
+            if 0 <= y+i < n and graph[y+i][x] == '|':
+                dfs(y+i, x)
+
+cnt = 0
+for y in range(n):
+    for x in range(m):
+        if graph[y][x] != 0:
+            dfs(y, x)
+            cnt += 1
+print(cnt)
